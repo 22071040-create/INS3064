e47110e3cd7d2effaa45ee5b43407ec92bfd755f
22071040-create <22071040@vnu.edu.vn>
2025-09-23 17:22:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
-@ -0,0 +1,16 @@
-<! DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
-    <meta charset="UTF-8" >
-‹title></title>
+        <meta charset="UTF-8">
+        <title></title>
 </head>
 <body>
-‹?php
-$name = "Mr. A" ;
-$age = 20;
-$courses = array (" Java", "C", "PHP") ;
-echo    "Name: ".$name .",",age: ".$age
-        "<br/>3rd course is: " . $courses [2] ;
-?>
-</body>
+    <?php
+    $name = "Mr. A";
+    $age = 20;
+    $courses = array("Java", "C", "PHP");
+    echo   "Name: " . $name . ", age: ".$age . 
+           "<br/>3rd course is: " . $courses[2];
+        ?>
+    </body>
 </html>
